008bb608c34d704f3410709cc49166a5be314373
Herman Gouw <hgouw@users.noreply.github.com>
2018-05-28 20:20:57 +1000

    Added home, employees and pageNotFound components

--- employeeManagement/src/app/app.module.ts
+++ employeeManagement/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
