072af4533d91c746bb80d3577d0e6db030123a97
Herman Gouw <hgouw@users.noreply.github.com>
2018-05-29 21:26:50 +1000

    Added home, employees and pageNotFound components

--- RoutingDemo/src/app/app.module.ts
+++ RoutingDemo/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
