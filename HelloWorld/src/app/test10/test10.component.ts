99bbeaf37f7ada0c022b9b71417b44a95858959e
Herman Gouw <hermangouw@gmail.com>
2018-05-31 15:13:02 +1000

    Added ngSwitch

--- HelloWorld/src/app/test10/test10.component.ts
+++ HelloWorld/src/app/test10/test10.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
     <p>
       test10 works!
     </p>
+    <div [ngSwitch]="color">
+      <div *ngSwitchCase="'red'">You picked red colour</div>
+      <div *ngSwitchCase="'green'">You picked green colour</div>
+      <div *ngSwitchCase="'blue'">You picked blue colour</div>
+      <div *ngSwitchDefault>Unknown colour</div>      
+    </div>
   `,
   styles: []
 })
 export class Test10Component implements OnInit {
+
+  public color = "blue";
 
   constructor() { }
 
