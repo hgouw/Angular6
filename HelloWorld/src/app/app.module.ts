8eb52b4c8a1a051ea19615d260a0f8e4008545bf
Herman Gouw <hermangouw@gmail.com>
2018-05-31 11:57:33 +1000

    Added test5 component

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 import { Test4Component } from './test4/test4.component';
+import { Test5Component } from './test5/test5.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { Test4Component } from './test4/test4.component';
     Test1Component,
     Test2Component,
     Test3Component,
-    Test4Component
+    Test4Component,
+    Test5Component
   ],
   imports: [
     BrowserModule
---
+++ HelloWorld/src/app/test5/test5.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test5',
+  template: `
+    <p>
+      test5 works!
+    </p>
+  `,
+  styles: []
+})
+export class Test5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
