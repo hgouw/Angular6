9fa46135332889caab6ff06b5903bebdff226ef2
Herman Gouw <hermangouw@gmail.com>
2018-05-31 15:32:05 +1000

    Added ngFor directive

--- HelloWorld/src/app/test11/test11.component.ts
+++ HelloWorld/src/app/test11/test11.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
     <p>
       test11 works!
     </p>
+    <div *ngFor="let colour of colours; index as i; first as f; last as l; odd as o; even as e">
+      <h2 [style.color]="colour">{{i}} - {{colour}} ({{f}}) ({{l}}) ({{o}}) ({{e}})</h2>
+    </div>
   `,
   styles: []
 })
 export class Test11Component implements OnInit {
+
+  public colours = ["red", "green", "blue"];
 
   constructor() { }
 
