182f8703fb39b90838d84e0f1503022dfff46f60
Herman Gouw <hermangouw@gmail.com>
2018-05-31 12:12:56 +1000

    Added Style Bindings

--- HelloWorld/src/app/test5/test5.component.ts
+++ HelloWorld/src/app/test5/test5.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
     <p>
       test5 works!
     </p>
+    <h2 [style.color]="'orange'">Hello World</h2>
+    <h2 [style.color]="hasError ? 'red' : 'blue'">Hello World</h2>
+    <h2 [style.color]="highlightColor">Hello World</h2>
+    <h2 [ngStyle]="titleStyles">Hello World</h2>
   `,
   styles: []
 })
 export class Test5Component implements OnInit {
+
+  public hasError = false;
+  public highlightColor = "orange";
+  public titleStyles = {
+    color: "brown",
+    fontStyle: "italic"
+  };
 
   constructor() { }
 
