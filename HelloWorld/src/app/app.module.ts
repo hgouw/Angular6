11b04bc86aec2ff26cb53ff78b978fa303623212
Herman Gouw <hermangouw@gmail.com>
2018-05-31 12:39:16 +1000

    Added test7 component

--- HelloWorld/src/app/app.module.ts
+++ HelloWorld/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { Test3Component } from './test3/test3.component';
 import { Test4Component } from './test4/test4.component';
 import { Test5Component } from './test5/test5.component';
 import { Test6Component } from './test6/test6.component';
+import { Test7Component } from './test7/test7.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { Test6Component } from './test6/test6.component';
     Test3Component,
     Test4Component,
     Test5Component,
-    Test6Component
+    Test6Component,
+    Test7Component
   ],
   imports: [
     BrowserModule
