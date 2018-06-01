0a2d025ebd33c7c8f88b15a8b0343263e22819d3
Herman Gouw <hermangouw@gmail.com>
2018-06-01 12:40:36 +1000

    Implemented employee-list component

--- Employee/src/app/employee-list/employee-list.component.ts
+++ Employee/src/app/employee-list/employee-list.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'employee-list',
   template: `
-    <p>
-      employee-list works!
-    </p>
+    <h2>Employee List</h2>
+    <ul *ngFor="let employee of employees">
+      <li>{{employee.name}}</li>
+    </ul>
   `,
   styles: []
 })
 export class EmployeeListComponent implements OnInit {
+
+  public employees = [
+    {"id": 1, "name": "Herman", "age": 57},
+    {"id": 2, "name": "Helen",  "age": 47},
+    {"id": 3, "name": "Sarah",  "age": 13},
+    {"id": 4, "name": "Olivia", "age": 9}
+  ];
 
   constructor() { }
 
