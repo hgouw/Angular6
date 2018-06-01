cbfc7f16321b1927ac9aa90a0dbf4db318ae0b0e
Herman Gouw <hermangouw@gmail.com>
2018-06-01 15:33:29 +1000

    Implemented HTTP error handling

--- Employee/src/app/employee-detail/employee-detail.component.ts
+++ Employee/src/app/employee-detail/employee-detail.component.ts
@@ -5,6 +5,7 @@ import { EmployeeService } from '../employee.service';
   selector: 'employee-detail',
   template: `
     <h2>Employee Detail</h2>
+    <h3>{{errorMsg}}</h3>
     <ul *ngFor="let employee of employees">
       <li>{{employee.id}}. {{employee.name}} - {{employee.age}}</li>
     </ul>
@@ -12,14 +13,14 @@ import { EmployeeService } from '../employee.service';
   styles: []
 })
 export class EmployeeDetailComponent implements OnInit {
-
   public employees = [];
+  public errorMsg;
 
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this._employeeService.getEmployees()
-        .subscribe(data => this.employees = data);
+        .subscribe(data => this.employees = data,
+                   error => this.errorMsg = error);
   }
-
 }
--- Employee/src/app/employee.service.ts
+++ Employee/src/app/employee.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { IEmployee } from './employee';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
   private _url: string = "/assets/data/employees.json";
 
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<IEmployee[]> {
-    return this.http.get<IEmployee[]>(this._url);
+    return this.http.get<IEmployee[]>(this._url).pipe(
+                    catchError(this.errorHandler));
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    return observableThrowError(error.message || "Server Error");
   }
 }
