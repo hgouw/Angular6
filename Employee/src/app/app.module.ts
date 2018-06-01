65944467237b3623c359039c784db1bef70e2569
Herman Gouw <hermangouw@gmail.com>
2018-06-01 12:42:30 +1000

    Added employee-detail component

--- Employee/src/app/app.module.ts
+++ Employee/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ Employee/src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'employee-detail',
+  template: `
+    <p>
+      employee-detail works!
+    </p>
+  `,
+  styles: []
+})
+export class EmployeeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
