df6a667b0a90deee806ada215651e7bd27c98a79
Herman Gouw <hgouw@users.noreply.github.com>
2018-06-01 21:03:08 +1000

    Added PageNotFound component

--- RoutingDemo2/src/app/app-routing.module.ts
+++ RoutingDemo2/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'departments', component: DepartmentListComponent },
   { path: 'employees', component: EmployeeListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
@@ -13,4 +15,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponents = [DepartmentListComponent, EmployeeListComponent];
+export const RoutingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent];
