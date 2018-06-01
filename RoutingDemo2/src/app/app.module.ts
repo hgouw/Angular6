1b0449e05f320ade9da69c769280815d7ea6b286
Herman Gouw <hermangouw@gmail.com>
2018-06-01 16:11:51 +1000

    Added EmployeeList component

--- RoutingDemo2/src/app/app.module.ts
+++ RoutingDemo2/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentListComponent
+    DepartmentListComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule
