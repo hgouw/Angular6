9fd830e86168105fe88fbec8beaa35bb4124222a
Herman Gouw <hgouw@users.noreply.github.com>
2018-06-01 22:08:22 +1000

    Implemented DepartmentList component

--- RoutingDemo2/src/app/department-list/department-list.component.ts
+++ RoutingDemo2/src/app/department-list/department-list.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+    <h3>Department List</h3>
+    <ul class="items">
+      <li *ngFor="let department of departments">
+        <span class="badge">{{department.id}}</span> {{department.name}}
+      </li>
+    </ul>
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
+
+  departments = [
+    { 'id': 1, 'name': 'COSC' },
+    { 'id': 2, 'name': 'Mathematics' },
+    { 'id': 3, 'name': 'Science' }
+  ]
 
   constructor() { }
 
