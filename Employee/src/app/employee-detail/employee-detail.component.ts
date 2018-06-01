e68269aa754441ab0dfabcad096f36f62c6868b3
Herman Gouw <hermangouw@gmail.com>
2018-06-01 12:46:03 +1000

    Implemented employee-detail component

--- Employee/src/app/employee-detail/employee-detail.component.ts
+++ Employee/src/app/employee-detail/employee-detail.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'employee-detail',
   template: `
-    <p>
-      employee-detail works!
-    </p>
+    <h2>Employee Detail</h2>
+    <ul *ngFor="let employee of employees">
+      <li>{{employee.id}}. {{employee.name}} - {{employee.age}}</li>
+    </ul>
   `,
   styles: []
 })
 export class EmployeeDetailComponent implements OnInit {
+
+  public employees = [
+    {"id": 1, "name": "Herman", "age": 57},
+    {"id": 2, "name": "Helen",  "age": 47},
+    {"id": 3, "name": "Sarah",  "age": 13},
+    {"id": 4, "name": "Olivia", "age": 9}
+  ];
 
   constructor() { }
 
