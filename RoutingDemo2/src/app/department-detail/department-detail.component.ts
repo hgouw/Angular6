07716e3f65d32f25af9da5cc6da6001c3b9a2f2c
Herman Gouw <hgouw@users.noreply.github.com>
2018-06-02 11:49:39 +1000

    Implemented DepartmentDetail component (snapshot not working)

--- RoutingDemo2/src/app/department-detail/department-detail.component.ts
+++ RoutingDemo2/src/app/department-detail/department-detail.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'department-detail',
   template: `
     <h3>You selected department with id: {{departmentId}}</h3>
+    <a (click)="goPrevious()" >Previous</a>
+    <a (click)="goNext()" >Next</a>
   `,
   styles: []
 })
 export class DepartmentDetailComponent implements OnInit {
 
-  public departmentId: Number;
+  public departmentId;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.departmentId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+  }
+
+  goPrevious() {
+    const previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+
+  goNext() {
+    const nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
   }
 
 }
