88e5afb6c069fe56c387ed14aa0bdb31f7dc60ce
Herman Gouw <hgouw@users.noreply.github.com>
2018-06-02 17:28:59 +1000

    Implemented Optional Route Parameters

--- RoutingDemo2/src/app/department-detail/department-detail.component.ts
+++ RoutingDemo2/src/app/department-detail/department-detail.component.ts
@@ -7,6 +7,9 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
     <h3>You selected department with id: {{departmentId}}</h3>
     <a (click)="goPrevious()" >Previous</a>
     <a (click)="goNext()" >Next</a>
+    <div>
+      <button (click)="gotoDepartments()" >Back</button>
+    </div>
   `,
   styles: []
 })
@@ -31,6 +34,11 @@ export class DepartmentDetailComponent implements OnInit {
   goNext() {
     const nextId = this.departmentId + 1;
     this.router.navigate(['/departments', nextId]);
+  }
+
+  gotoDepartments() {
+    const selectedId = this.departmentId ? this.departmentId : null;
+    this.router.navigate(['/departments', {id: selectedId}]);
   }
 
 }
--- RoutingDemo2/src/app/department-list/department-list.component.ts
+++ RoutingDemo2/src/app/department-list/department-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'department-list',
   template: `
     <h3>Department List</h3>
     <ul class="items">
-      <li (click)="onSelect(department)" *ngFor="let department of departments">
+      <li (click)="onSelect(department)" [class.selected]="isSelected(department)" *ngFor="let department of departments">
         <span class="badge">{{department.id}}</span> {{department.name}}
       </li>
     </ul>
@@ -15,19 +15,28 @@ import { Router } from '@angular/router';
 })
 export class DepartmentListComponent implements OnInit {
 
+  public selectedId;
+
   departments = [
     { 'id': 1, 'name': 'COSC' },
     { 'id': 2, 'name': 'Mathematics' },
     { 'id': 3, 'name': 'Science' }
   ];
 
-  constructor(private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.selectedId = parseInt(params.get('id'), 10);
+    });
   }
 
   onSelect(department) {
-      this.router.navigate(['/departments', department.id]);
+    this.router.navigate(['/departments', department.id]);
+  }
+
+  isSelected(department) {
+    return department.Id === this.selectedId;
   }
 
 }
